7ef31f6a901f656559fe384ddc52cc6c6252ecf9
hendricksonweib <hendricksonrocha22@gmail.com>
2024-07-28 16:01:26 -0300

    feat: text
    Signed-off-by: hendricksonweib <hendricksonrocha22@gmail.com>

--- src/template/NAV/Nav.tsx
+++ src/template/NAV/Nav.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Logo from "../../../public/Logo.svg";
 import { Link } from 'react-router-dom';
-import './Nav.css'; 
+import './Nav.css';
 
 const Nav: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,22 +11,20 @@ const Nav: React.FC = () => {
   };
 
   return (
-    <>
-      <header>
-        <img  className='logo' src={Logo} alt="macultura.svg" />
-        <div className={`menu-icon ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
-          <div className="menu-icon__line"></div>
-          <div className="menu-icon__line"></div>
-          <div className="menu-icon__line"></div>
-        </div>
-        <nav className={`nav-items ${isOpen ? 'open' : ''}`}>
-        <Link to="/culinaria" className="nav-items" onClick={toggleMenu}>Culinária</Link>
-        <Link to="/festas" className="nav-items" onClick={toggleMenu}>Festas</Link>
-        <Link to="/artesanato" className="nav-items" onClick={toggleMenu}>Artesanato</Link>
-        <Link to="/turismo" className="nav-items" onClick={toggleMenu}>Turismo</Link>
-        </nav>
-      </header>
-    </>
+    <header>
+      <img className='logo' src={Logo} alt="macultura.svg" />
+      <div className={`menu-icon ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <div className="menu-icon__line"></div>
+        <div className="menu-icon__line"></div>
+        <div className="menu-icon__line"></div>
+      </div>
+      <nav className={`nav-items ${isOpen ? 'open' : ''}`}>
+        <Link to="/culinaria" className="nav-link" onClick={toggleMenu}>Culinária</Link>
+        <Link to="/festas" className="nav-link" onClick={toggleMenu}>Festas</Link>
+        <Link to="/artesanato" className="nav-link" onClick={toggleMenu}>Artesanato</Link>
+        <Link to="/turismo" className="nav-link" onClick={toggleMenu}>Turismo</Link>
+      </nav>
+    </header>
   );
 };
 
